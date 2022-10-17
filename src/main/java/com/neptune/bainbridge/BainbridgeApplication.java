93816284b9697e7e34892b481555120800048eb0
arun-lalithambaran <arun32me@gmail.com>
2022-10-17 22:13:56 +0530

    project init

---
+++ src/main/java/com/neptune/bainbridge/BainbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.neptune.bainbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BainbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BainbridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neptune/bainbridge/BainbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neptune.bainbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BainbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
